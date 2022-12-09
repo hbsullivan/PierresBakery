e82553801a4a73d4d754058974cafb9af0ae9ba7
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-09 09:19:12 -0800

    PASS: Returns correct price if user buys 1 or 2 loaves of bread

--- PierreBakery/Models/Bakery.cs
+++ PierreBakery/Models/Bakery.cs
@@ -2,16 +2,34 @@ namespace PierreBakery
 {
   public class Bread
   {
-    public int BreadNumber { get; }
+    public int BreadNumber { get; set; }
 
     public Bread(string stringBreadNumber)
     {
       BreadNumber = int.Parse(stringBreadNumber);
     }
+
+    public int BreadPrice()
+    {
+      int counter = 0;
+      for (int i=0; i < BreadNumber; i++)
+      {
+        if (i < 3)
+        {
+          counter += 5;
+        }
+      }
+      return counter;
+    }
   }
 
   public class Pastry
   {
-    
+    public int PastryNumber { get; set; }
+
+    public Pastry(string stringPastryNumber)
+    {
+      PastryNumber = int.Parse(stringPastryNumber);
+    }
   }
 }
