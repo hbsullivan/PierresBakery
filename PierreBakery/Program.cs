3f59dc80d03e03e862631ad97e3378e8471b0160
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-09 13:55:39 -0800

    add ui for returning price of pastries

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -9,10 +9,19 @@ namespace UserInterface
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("We have Bread and Pastries available");
+
       Console.WriteLine("How many loaves of bread do you want?");
       string stringBreadNumber = Console.ReadLine();
       Bread newBread = new Bread(stringBreadNumber);
       Console.WriteLine("The price of your bread is $" + newBread.BreadPrice());
+
+      Console.WriteLine("How many pastries would you like?");
+      string stringPastryNumber = Console.ReadLine();
+      Pastry newPastry = new Pastry(stringPastryNumber);
+      Console.WriteLine("The price of your pastries is $" + newPastry.PastryPrice());
+
+      
+
       
     }
   }
