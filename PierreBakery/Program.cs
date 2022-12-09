0004427fd97e7b41d10e502dc9f7c841082bcc40
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-09 13:25:36 -0800

    UI now returns the price of how many loaves of bread the user enters

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -7,6 +7,12 @@ namespace UserInterface
   {
     public static void Main()
     {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("We have Bread and Pastries available");
+      Console.WriteLine("How many loaves of bread do you want?");
+      string stringBreadNumber = Console.ReadLine();
+      Bread newBread = new Bread(stringBreadNumber);
+      Console.WriteLine("The price of your bread is $" + newBread.BreadPrice());
       
     }
   }
