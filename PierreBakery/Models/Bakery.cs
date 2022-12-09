62554767ed3576ebaee9b5fc6603c28cadbdc721
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-09 08:22:48 -0800

    PASS: creates Bread class

--- PierreBakery/Models/Bakery.cs
+++ PierreBakery/Models/Bakery.cs
@@ -2,11 +2,6 @@ namespace PierreBakery
 {
   public class Bread
   {
-    // properties, constructors, methods, etc. go here
-  }
-
-  public class Pastry
-  {
-    // properties, constructors, methods, etc. go here
+    
   }
 }
